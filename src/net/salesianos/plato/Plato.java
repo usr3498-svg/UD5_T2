1ce71008c6fced8ec75d210eddb344c221bb5470
usr3498-svg <usr3498@salesianos-lacuesta.net>
2026-04-17 15:26:06 +0100

    Update plato.java
    Creamos la clase plato definiendo sus atributos, constructor, y como metodos de acceso son los getters y los setters.

--- src/net/salesianos/plato/Plato.java
+++ src/net/salesianos/plato/Plato.java
@@ -1,4 +1,49 @@
 package net.salesianos.plato;
+
 public class Plato {
-    
+    private String nombre;
+    private String tipoPez;
+    private int precio;
+    private int puntuacion;
+
+    public Plato(String nombre, String tipoPez, int precio, int puntuacion) {
+        this.nombre = nombre;
+        this.tipoPez = tipoPez;
+        this.precio = precio;
+        this.puntuacion = puntuacion;
+    }
+
+    public String getNombre()  {
+        return nombre;
+    }
+
+    public String getTipoPez(){
+
+        return tipoPez;
+    }
+
+    public int getPrecio(){
+        return precio;
+    }
+    public int getPuntuacion(){
+        return puntuacion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTipoPez(String tipoPez) {
+        this.tipoPez = tipoPez;
+    }
+
+    public void setPrecio(int precio){
+        this.precio = precio;
+    }
+
+    public void setPuntuacion(int puntuacion){
+        this.puntuacion = puntuacion;
+    }
+
+
 }
--- src/net/salesianos/plato/ServicioPlato.java
+++ src/net/salesianos/plato/ServicioPlato.java
@@ -1,5 +1,4 @@
 package net.salesianos.plato;
-
 public class ServicioPlato {
     
 }
