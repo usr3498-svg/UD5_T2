d858a866933606a4736fba1b37c92f09dc42b847
usr3498-svg <usr3498@salesianos-lacuesta.net>
2026-04-17 16:00:13 +0100

    Update ServicioPlato.java
    Creo arraylist de platos, opcion de añadir platos, opcion de eliminar platos, opcion de cambiar el orden en la arraylist y la opcion de que devuelva todo el listado de platos.

--- src/net/salesianos/plato/ServicioPlato.java
+++ src/net/salesianos/plato/ServicioPlato.java
@@ -1,4 +1,25 @@
 package net.salesianos.plato;
+import java.util.ArrayList;
+
 public class ServicioPlato {
+
+    ArrayList<Plato> platos = new ArrayList<>();
+
+    public void add(Plato p) {
+        platos.add(p);
+    }
+
+    public void remove(int i) {
+        platos.remove(i);
+    }
+
+    public void update(int i, Plato p) {
+        platos.set(i, p);
+    }
+
+    public ArrayList<Plato> getAll() {
+        return platos;
+    }
+
     
 }
