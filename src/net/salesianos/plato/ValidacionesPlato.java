b4d1666080274be194ce3917b74b7806f3d72a90
usr3498-svg <usr3498@salesianos-lacuesta.net>
2026-04-17 16:19:25 +0100

    Update ValidacionesPlato.java
    Creo validaciones de texto, número y puntuación

--- src/net/salesianos/plato/ValidacionesPlato.java
+++ src/net/salesianos/plato/ValidacionesPlato.java
@@ -1,5 +1,35 @@
 package net.salesianos.plato;
 
 public class ValidacionesPlato {
-    
+
+    public static String texto(String texto) {
+        if (texto == null) {
+            return null;
+        } else {
+            return texto;
+        }
+    }
+
+    public static int numero(String num) {
+
+        String[] numeros = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i].equals(num)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int puntuacion(String num) {
+        int punt = numero(num);
+
+        if (punt < 1 || punt > 5) {
+            return -1;
+        }
+
+        return punt;
+    }
 }
